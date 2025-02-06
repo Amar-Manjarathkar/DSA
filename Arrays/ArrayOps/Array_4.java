22913bf37e5baebd35a057dfc444ccc361e107c3
Amar Manjarathkar <amar.manjarathkar@dypic.com>
2025-02-06 21:48:42 +0530

    second commit

--- Arrays/ArrayOps/Array_4.java
+++ Arrays/ArrayOps/Array_4.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Array_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the element u want to search ");
+
+        System.out.print("Enter the element you want to search: ");
         int element = sc.nextInt();
 
-        int arr [] = {10,20,85,45,96};
+        int[] arr = {10, 20, 85, 45, 96};
+        boolean found = false;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]==element) {
-                System.out.println("Element is present at " + i +" Index");   
-            } 
+            if (arr[i] == element) {
+                System.out.println("Element is present at index " + i);
+                found = true;
+                break;  // Exit loop early if element is found
+            }
         }
-
+        if (!found) {
+            System.out.println("Element not found in the array.");
+        }
+        sc.close();  // Close the Scanner
     }
     
 }
