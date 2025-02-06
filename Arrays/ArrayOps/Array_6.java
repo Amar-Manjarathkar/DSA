f52641934562640365f4415d727131aacd23ad0b
Amar Manjarathkar <amar.manjarathkar@dypic.com>
2025-02-06 22:13:43 +0530

    First Commit

--- Arrays/ArrayOps/Array_6.java
+++ Arrays/ArrayOps/Array_6.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Array_6 {
     public static void main(String[] args) {
+        // Arrays largest element using JAVA 8 Stream 
         int arr [] = {10000,20,45000,6465};
         int max = Arrays.stream(arr).max().getAsInt(); 
         System.out.println("The largest element is "+max);
---
+++ Arrays/ArrayOps/Array_7.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Array_7 {
+    public static void main(String[] args) {
+        // sort and print the last element of the sorted array 
+        int arr1[] = { 65400,552,76984,23165,464};
+        Arrays.sort(arr1);
+        System.out.print("Largest element is "+ arr1[arr1.length -1]);
+    }
+    
+}
